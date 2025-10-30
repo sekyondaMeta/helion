182d16f19261094a75ae7ae5c2535cc364b97b3e
Will Feng <yfeng.us@gmail.com>
2025-10-29 17:46:43 -0700

    [CI] Fix debug_str() to be compatible with latest PyTorch nightly (#1050)

--- helion/_compiler/device_ir.py
+++ helion/_compiler/device_ir.py
@@ -300,7 +300,8 @@ class DeviceIR:
 
     def debug_str(self) -> str:
         result = str(self)
-        return re.sub(r" ?(# File:\s+).*/([^/:]+:\d+)", r"\1.../\2", result)
+        # Normalize indentation to 4 spaces to handle both PyTorch 2.9 and nightly formatting
+        return re.sub(r" *(# File:\s+).*/([^/:]+:\d+)", r"    \1.../\2", result)
 
     def add_graph(
         self,
